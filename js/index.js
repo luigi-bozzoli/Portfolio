9652b975ccfb3ae25348beb14981e08fc3db3835
Luigi <luigibozzoli@studenti.unisa.it>
2021-07-01 19:02:03 +0200

    added projects page

--- js/index.js
+++ js/index.js
@@ -1,158 +1,105 @@
-var canvas = document.createElement("canvas");
-var width = canvas.width = window.innerWidth * 0.75;
-var height = canvas.height = window.innerHeight * 0.75;
-document.body.appendChild(canvas);
-var gl = canvas.getContext('webgl');
+// ParticlesJS Config.
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 700 } },
 
-var mouse = {x: 0, y: 0};
 
-var viewPortHeight = $(window).height();
-var viewPortWidth = $(window).width();
-const DIVIDER = 50000;
-var numMetaballs = Math.trunc((viewPortHeight * viewPortWidth)/DIVIDER);
-console.log(numMetaballs);
-var metaballs = [];
+    "color": {
+      "value": "#ffffff" },
 
-for (var i = 0; i < numMetaballs; i++) {
-  var radius = Math.random() * 60 + 10;
-  metaballs.push({
-    x: Math.random() * (width - 2 * radius) + radius,
-    y: Math.random() * (height - 2 * radius) + radius,
-    vx: (Math.random() - 0.5) * 3,
-    vy: (Math.random() - 0.5) * 3,
-    r: radius * 0.75
-  });
-}
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#ffffff" },
 
-var vertexShaderSrc = `
-attribute vec2 position;
+      "polygon": {
+        "nb_sides": 5 } },
 
-void main() {
-// position specifies only x and y.
-// We set z to be 0.0, and w to be 1.0
-gl_Position = vec4(position, 0.0, 1.0);
-}
-`;
 
-var fragmentShaderSrc = `
-precision highp float;
+    "opacity": {
+      "value": 0.75,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 0.1,
+        "opacity_min": 0.1,
+        "sync": false } },
 
-const float WIDTH = ` + (width >> 0) + `.0;
-const float HEIGHT = ` + (height >> 0) + `.0;
 
-uniform vec3 metaballs[` + numMetaballs + `];
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "size_min": 0.25,
+        "sync": false } },
 
-void main(){
-float x = gl_FragCoord.x;
-float y = gl_FragCoord.y;
 
-float sum = 0.0;
-for (int i = 0; i < ` + numMetaballs + `; i++) {
-vec3 metaball = metaballs[i];
-float dx = metaball.x - x;
-float dy = metaball.y - y;
-float radius = metaball.z;
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1 },
 
-sum += (radius * radius) / (dx * dx + dy * dy);
-}
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "bounce",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 3000,
+        "rotateY": 1500 } } },
 
-if (sum >= 0.99) {
-gl_FragColor = vec4(mix(vec3(x / WIDTH, y / HEIGHT, 1.0), vec3(0, 0, 0), max(0.0, 1.0 - (sum - 0.99) * 100.0)), 1.0);
-return;
-}
 
-gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-}
 
-`;
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab" },
 
-var vertexShader = compileShader(vertexShaderSrc, gl.VERTEX_SHADER);
-var fragmentShader = compileShader(fragmentShaderSrc, gl.FRAGMENT_SHADER);
+      "onclick": {
+        "enable": true,
+        "mode": "push" },
 
-var program = gl.createProgram();
-gl.attachShader(program, vertexShader);
-gl.attachShader(program, fragmentShader);
-gl.linkProgram(program);
-gl.useProgram(program);
+      "resize": true },
 
-var vertexData = new Float32Array([
-  -1.0,  1.0, // top left
-  -1.0, -1.0, // bottom left
-  1.0,  1.0, // top right
-  1.0, -1.0, // bottom right
-]);
-var vertexDataBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1 } },
 
-var positionHandle = getAttribLocation(program, 'position');
-gl.enableVertexAttribArray(positionHandle);
-gl.vertexAttribPointer(positionHandle,
-                       2, // position is a vec2
-                       gl.FLOAT, // each component is a float
-                       gl.FALSE, // don't normalize values
-                       2 * 4, // two 4 byte float components per vertex
-                       0 // offset into each span of vertex data
-                      );
 
-var metaballsHandle = getUniformLocation(program, 'metaballs');
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3 },
 
-loop();
-function loop() {
-  for (var i = 0; i < numMetaballs; i++) {
-    var metaball = metaballs[i];
-    metaball.x += metaball.vx;
-    metaball.y += metaball.vy;
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4 },
 
-    if (metaball.x < metaball.r || metaball.x > width - metaball.r) metaball.vx *= -1;
-    if (metaball.y < metaball.r || metaball.y > height - metaball.r) metaball.vy *= -1;
-  }
+      "push": {
+        "particles_nb": 4 },
 
-  var dataToSendToGPU = new Float32Array(3 * numMetaballs);
-  for (var i = 0; i < numMetaballs; i++) {
-    var baseIndex = 3 * i;
-    var mb = metaballs[i];
-    dataToSendToGPU[baseIndex + 0] = mb.x;
-    dataToSendToGPU[baseIndex + 1] = mb.y;
-    dataToSendToGPU[baseIndex + 2] = mb.r;
-  }
-  gl.uniform3fv(metaballsHandle, dataToSendToGPU);
+      "remove": {
+        "particles_nb": 2 } } },
 
-  //Draw
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
-  requestAnimationFrame(loop);
-}
 
-function compileShader(shaderSource, shaderType) {
-  var shader = gl.createShader(shaderType);
-  gl.shaderSource(shader, shaderSource);
-  gl.compileShader(shader);
-
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    throw "Shader compile failed with: " + gl.getShaderInfoLog(shader);
-  }
-
-  return shader;
-}
-
-function getUniformLocation(program, name) {
-  var uniformLocation = gl.getUniformLocation(program, name);
-  if (uniformLocation === -1) {
-    throw 'Can not find uniform ' + name + '.';
-  }
-  return uniformLocation;
-}
-
-function getAttribLocation(program, name) {
-  var attributeLocation = gl.getAttribLocation(program, name);
-  if (attributeLocation === -1) {
-    throw 'Can not find attribute ' + name + '.';
-  }
-  return attributeLocation;
-}
-
-canvas.onmousemove = function(e) {
-  mouse.x = e.clientX;
-  mouse.y = e.clientY;
-}
+  "retina_detect": true });
